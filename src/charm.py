21f49a6dcd9fb193dafefe989511b5bcc17d0508
Carl Csaposs <carl.csaposs@canonical.com>
2024-06-05 13:54:12 +0200

    info log each event

--- src/charm.py
+++ src/charm.py
@@ -24,6 +24,18 @@ class FooCharm(ops.CharmBase):
 
     def __init__(self, *args):
         super().__init__(*args)
+        self.framework.observe(self.on.install, self._on_event)
+        self.framework.observe(self.on.config_changed, self._on_event)
+        self.framework.observe(self.on.leader_elected, self._on_event)
+        self.framework.observe(self.on.leader_settings_changed, self._on_event)
+        self.framework.observe(self.on.start, self._on_event)
+        self.framework.observe(self.on.update_status, self._on_event)
+        self.framework.observe(self.on.upgrade_charm, self._on_event)
+        self.framework.observe(self.on.stop, self._on_event)
+        self.framework.observe(self.on.remove, self._on_event)
+
+    def _on_event(self, event: ops.EventBase):
+        logger.info(f"Event: {type(event)}")
 
 if __name__ == "__main__":  # pragma: nocover
     ops.main(FooCharm)  # type: ignore
